0e1091a592fbbfc85d2a14d5ee070f5fa4be7b38
DONGBUM, KIM <dongbum9@gmail.com>
2019-07-12 00:17:45 +0900

    DB 설정 정보 가져오지 못할시 에러처리 추가

--- bikeparking/__init__.py
+++ bikeparking/__init__.py
@@ -33,11 +33,15 @@ def create_app(config_filepath='resource/config.cfg'):
     bikeparking_app.config.from_pyfile(config_filepath, silent=True)
     print_settings(bikeparking_app.config.items())
 
-    db_address = bikeparking_app.config['DB_ADDRESS']
-    db_port = bikeparking_app.config['DB_PORT']
-    db_id = bikeparking_app.config['DB_ID']
-    db_password = bikeparking_app.config['DB_PASSWORD']
-    db_name = bikeparking_app.config['DB_NAME']
+    try:
+        db_address = bikeparking_app.config['DB_ADDRESS']
+        db_port = bikeparking_app.config['DB_PORT']
+        db_id = bikeparking_app.config['DB_ID']
+        db_password = bikeparking_app.config['DB_PASSWORD']
+        db_name = bikeparking_app.config['DB_NAME']
+    except ValueError:
+        print("Cannot found DB connection info.")
+        exit(1)
 
     from bikeparking.database import DBManager
     DBManager.init(db_address, db_port, db_id, db_password, db_name)
