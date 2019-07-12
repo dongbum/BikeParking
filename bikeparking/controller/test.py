3131ed1f474c21ef26de5662b54e2d45bf152a0c
DONGBUM, KIM <dongbum9@gmail.com>
2019-07-12 10:55:09 +0900

    SQLAlchemy 관련 내용 모두 삭제

--- bikeparking/controller/test.py
+++ bikeparking/controller/test.py
@@ -1,11 +1,8 @@
 # -*- coding: utf-8 -*-
 
-from bikeparking import *
+from flask import render_template
 from bikeparking.bikeparking_blueprint import bikeparking
-from bikeparking.database import dao
-from bikeparking.model.parking import Parking
 
 @bikeparking.route('/test')
 def test():
-    bikeparking_data = dao
-    return render_template('test.html', bikeparking=bikeparking_data)
+    return render_template('test.html')
